15b5254c865a8013bcb5f59d721d7b632dc315c9
flavien <flavien.pensato@gmail.com>
2014-10-30 15:22:22 +0100

    Submit work perfectly

--- client/templates/bookmark/bookmark_submit.js
+++ client/templates/bookmark/bookmark_submit.js
@@ -6,22 +6,39 @@ Template.bookmarkSubmit.events(
 
 		var user = Meteor.user();
 
-		var bookmark = 
+		if (user !== null)
 		{
-			url: $(events.target).find('[name=url]').val(),
-			title: $(events.target).find('[name=title]').val(),
-			summuary: $(events.target).find('[name=summuary]').val(),
-			thumbnail: "",
-			tag: "",
-			dateCreated: new Date(),
-			dateModified: new Date(),
-			counterView: 0,
-			counterClick: 0,
-			versionText: "",
-			idUser: user._id
-		};
+			var bookmark = 
+			{
+				url: $(events.target).find('[name=url]').val(),
+				title: $(events.target).find('[name=title]').val(),
+				summuary: $(events.target).find('[name=summuary]').val(),
+				thumbnail: "",
+				tag: "",
+				dateCreated: new Date(),
+				dateModified: new Date(),
+				counterView: 0,
+				counterClick: 0,
+				versionText: "",
+				idUser: user._id
+			};
 
-		Bookmarks.insert(bookmark);
-		Router.go('/');
+			if (bookmark.url !== "")
+			{
+				Bookmarks.insert(bookmark);
+				Router.go('/');
+			}
+			else
+			{
+				document.getElementById("spanUrl").style.visibility = "visible";
+				document.getElementById("url").focus();
+			}			
+		}
+		else
+		{
+			var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>You need to be connected.</p></div>');
+			$("#alertConnexion").append(e);
+			e.attr('id', 'myid');
+		}
 	}
 });
