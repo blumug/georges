36e5b580ba633a978ee24c92b70b170deee83d5c
flavien <flavien.pensato@gmail.com>
2014-10-31 17:39:11 +0100

    Fix bug of the last commit

--- client/templates/bookmark/bookmarks_list.js
+++ client/templates/bookmark/bookmarks_list.js
@@ -1,20 +1,9 @@
 Template.bookmarksList.helpers({
     bookmarks: function() {
         if (Meteor.user()) {
-            var searchTags = document.getElementById("tagSearch").value;
-            if (searchTags != undefined) {
-                return Bookmarks.find({
-                    tags: searchTags
-                }, {
-                    sort: {
-                        dateCreated: -1
-                    }
-                })
-            } else {
-                return (Bookmarks.find({
-                    userId: Meteor.user()._id
-                }))
-            }
+            return (Bookmarks.find({
+                userId: Meteor.user()._id
+            }))
         }
     }
 });
