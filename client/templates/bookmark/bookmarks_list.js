8b0e88a688525e1f797aa20b2c467e7f4d5cc081
Jérôme Blondon <jerome@blondon.fr>
2014-11-01 06:13:48 +0100

    fixed empty search

--- client/templates/bookmark/bookmarks_list.js
+++ client/templates/bookmark/bookmarks_list.js
@@ -2,7 +2,7 @@ Template.bookmarksList.helpers({
     bookmarks: function() {
         var filter = Session.get('searchBar');
         if (Meteor.user()) {
-            if (filter === undefined) {
+            if (filter === undefined || filter === '') {
                 return (Bookmarks.find({
                     userId: Meteor.user()._id
                 }));
