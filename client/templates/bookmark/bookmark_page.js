91fd08890de59feae11b6804951d5d64b20207fa
flavien <flavien.pensato@gmail.com>
2014-11-06 11:32:58 +0100

    Secure call server + put function parse in lib/tag_utils.js

--- client/templates/bookmark/bookmark_page.js
+++ client/templates/bookmark/bookmark_page.js
@@ -3,7 +3,7 @@ Template.bookmarkPage.events({
         events.preventDefault();
         var user = Meteor.user();
         if (user !== null) {
-            var tags = parsedTags($(events.target).find('[name=tags]').val());
+            var tags = ParsedTags($(events.target).find('[name=tags]').val());
             var bookmark = {
                 url: $(events.target).find('[name=url]').val(),
                 title: $(events.target).find('[name=title]').val(),
@@ -19,10 +19,21 @@ Template.bookmarkPage.events({
                 userId: user._id
             };
             if (bookmark.url.indexOf("://") == -1) bookmark.url = "http://" + bookmark.url;
-            createdTags(tags);
-            Bookmarks.remove(this._id);
-            Bookmarks.insert(bookmark);
-            Router.go('/');
+            CreatedTags(tags);
+            if (this.url == bookmark.url || Bookmarks.find({
+                url: bookmark.url,
+                userId: bookmark.userId
+            }).count() == 0) {
+                Meteor.call('removeBookmark', this._id);
+                Meteor.call('bookmarkUpdate', bookmark, function(error, result) {
+                    if (error) return alert(error.reason);
+                    Router.go('/');
+                });
+            } else {
+                var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>Url already use.</p></div>');
+                $("#alertConnexion").append(e);
+                e.attr('id', 'myid');
+            }
         } else {
             var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>You need to be connected.</p></div>');
             $("#alertConnexion").append(e);
@@ -40,45 +51,3 @@ Handlebars.registerHelper("prettifyDate", function(timestamp) {
 Handlebars.registerHelper("prettifyTags", function(tags) {
     return tags.join(" ");
 });
-// parse the input "tags"
-// return a list of string (tags)
-// ex: "#unpeu #beaucoup #alafolie"
-//  |=> [ "#unpeu", "#beaucoup", #alafolie" ]
-var parsedTags = function(tags) {
-    var parsedTags = [];
-    var i = 0;
-    var x = 0;
-    var y;
-    while (tags !== "" && y != -1) {
-        x = tags.indexOf("#");
-        y = x++;
-        while (y != -1 && x < tags.length && tags[x] != ' ' && tags[x] != '#') x++;
-        if (y != -1 && (tags[x] == ' ' || tags[x] == '#' || x >= tags.length)) {
-            parsedTags[i++] = tags.substr(y, x);
-            tags = tags.substr(x);
-        }
-    }
-    return (parsedTags);
-}
-// created new Element od collection Tags
-// or up the value counterWrite of the element
-// return void
-var createdTags = function(arrayTags) {
-    var x = 0;
-    var newTag = {
-        name: "",
-        counterWrite: 1
-    };
-    while (x < arrayTags.length) {
-        var tag = Tags.findOne({
-            name: arrayTags[x]
-        });
-        if (tag == undefined) {
-            newTag.name = arrayTags[x]
-            Tags.insert(newTag);
-        } else {
-            Meteor.call("upCounterWrite", tag._id);
-        }
-        x++;
-    }
-}
--- client/templates/bookmark/bookmark_submit.js
+++ client/templates/bookmark/bookmark_submit.js
@@ -3,38 +3,25 @@ Template.bookmarkSubmit.events({
         events.preventDefault();
         var user = Meteor.user();
         if (user !== null) {
-            var tags = parsedTags($(events.target).find('[name=tags]').val());
+            var tags = ParsedTags($(events.target).find('[name=tags]').val());
             var bookmark = {
                 url: $(events.target).find('[name=url]').val(),
                 title: $(events.target).find('[name=title]').val(),
                 description: $(events.target).find('[name=description]').val(),
-                thumbnail: "",
                 tags: tags,
-                dateCreated: new Date(),
-                dateModified: new Date(),
-                counterView: 0,
-                counterClick: 0,
-                summary: "",
-                versionText: "",
                 userId: user._id
             };
             if (bookmark.url.indexOf("://") == -1) bookmark.url = "http://" + bookmark.url;
-            if (Bookmarks.findOne({
-                url: bookmark.url,
-                userId: user._id
-            }) !== undefined) {
-                var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>This url does already exist in your list.</p></div>');
-                $("#alertConnexion").append(e);
-                e.attr('id', 'myid');
-            } else {
-                createdTags(tags);
-                Meteor.call('bookmarkInsert', bookmark, function(error, result) {
-                    if (error) return alert(error.reason);
-                    Router.go('/');
-                });
-                // Bookmarks.insert(bookmark);
-                // Router.go('/');
-            }
+            CreatedTags(tags);
+            Meteor.call('bookmarkInsert', bookmark, function(error, result) {
+                if (error) return alert(error.reason);
+                if (result.bookmarkExists) {
+                    var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>This url does already exist in your list.</p></div>');
+                    $("#alertConnexion").append(e);
+                    e.attr('id', 'myid');
+                }
+                Router.go('/');
+            });
         } else {
             var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>You need to be connected.</p></div>');
             $("#alertConnexion").append(e);
@@ -42,45 +29,3 @@ Template.bookmarkSubmit.events({
         }
     }
 });
-// parse the input "tags"
-// return a list of string (tags)
-// ex: "#unpeu #beaucoup #alafolie"
-//  |=> [ "#unpeu", "#beaucoup", #alafolie" ]
-var parsedTags = function(tags) {
-    var parsedTags = [];
-    var i = 0;
-    var x = 0;
-    var y;
-    while (tags !== "" && y != -1) {
-        x = tags.indexOf("#");
-        y = x++;
-        while (y != -1 && x < tags.length && tags[x] != ' ' && tags[x] != '#') x++;
-        if (y != -1 && (tags[x] == ' ' || tags[x] == '#' || x >= tags.length)) {
-            parsedTags[i++] = tags.substr(y, x);
-            tags = tags.substr(x);
-        }
-    }
-    return (parsedTags);
-}
-// created new Element od collection Tags
-// or up the value counterWrite of the element
-// return void
-var createdTags = function(arrayTags) {
-    var x = 0;
-    var newTag = {
-        name: "",
-        counterWrite: 1
-    };
-    while (x < arrayTags.length) {
-        var tag = Tags.findOne({
-            name: arrayTags[x]
-        });
-        if (tag == undefined) {
-            newTag.name = arrayTags[x]
-            Tags.insert(newTag);
-        } else {
-            Meteor.call("upCounterWrite", tag._id);
-        }
-        x++;
-    }
-}
