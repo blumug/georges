4216a18346e2ffb153e4ad82294b6be1dcf188d0
flavien <flavien.pensato@gmail.com>
2014-11-06 16:57:16 +0100

    Adding pagination

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
-Meteor.publish('bookmarks', function() {
-    return Bookmarks.find();
+Meteor.publish('bookmarks', function(options) {
+  return Bookmarks.find({}, options);
 });
 Meteor.publish('tags', function() {
     return Tags.find();
