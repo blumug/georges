1ae6c36c0c58647ea29f0797da97b26e2ce661ec
flavien <flavien.pensato@gmail.com>
2014-11-07 10:09:40 +0100

    Adding bookmark publication

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
-Meteor.publish('bookmarks', function() {
-  return Bookmarks.find({});
+Meteor.publish('bookmarks', function(option) {
+    return Bookmarks.find({userId: this.userId}, option);
+});
+Meteor.publish('bookmark', function(id) {
+    check(id, String);
+    return Bookmarks.find(id);
 });
 Meteor.publish('tags', function() {
     return Tags.find();
