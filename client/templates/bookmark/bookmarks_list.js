3d9ff1e1a0411d3fd6c6825571c3cd13d7d5842d
flavien <flavien.pensato@gmail.com>
2014-11-07 11:03:31 +0100

    Adding search publication

--- client/templates/bookmark/bookmarks_list.js
+++ client/templates/bookmark/bookmarks_list.js
@@ -2,38 +2,39 @@ Template.bookmarksList.helpers({
     bookmarks: function() {
         var filter = Session.get('searchBar');
         if (Meteor.user()) {
-            if (filter === undefined || filter === '') {
-                return (Bookmarks.find({
-                    userId: Meteor.user()._id
-                }));
-            } else {
-                if (filter.indexOf("#") != -1) {
-                    var parsedTags = ParsedTags(filter);
-                    return Bookmarks.find({
-                        tags: {
-                            $in: parsedTags
-                        }
-                    }, {
-                        sort: {
-                            dateCreated: -1
-                        }
-                    })
-                } else {
-                    return (Bookmarks.find({
-                        $or: [{
-                            $where: "this.url.indexOf(\"" + filter + "\") != -1"
-                        }, {
-                            $where: "this.title.indexOf(\"" + filter + "\") != -1"
-                        }, {
-                            $where: "this.description.indexOf(\"" + filter + "\") != -1"
-                        }]
-                    }, {
-                        sort: {
-                            dateCreated: -1
-                        }
-                    }));
-                }
-            }
+            Router.go('/search/' + filter);
+            // if (filter === undefined || filter === '') {
+            //     return (Bookmarks.find({
+            //         userId: Meteor.user()._id
+            //     }));
+            // } else {
+            //     if (filter.indexOf("#") != -1) {
+            //         var parsedTags = ParsedTags(filter);
+            //         return Bookmarks.find({
+            //             tags: {
+            //                 $in: parsedTags
+            //             }
+            //         }, {
+            //             sort: {
+            //                 dateCreated: -1
+            //             }
+            //         })
+            //     } else {
+            //         return (Bookmarks.find({
+            //             $or: [{
+            //                 $where: "this.url.indexOf(\"" + filter + "\") != -1"
+            //             }, {
+            //                 $where: "this.title.indexOf(\"" + filter + "\") != -1"
+            //             }, {
+            //                 $where: "this.description.indexOf(\"" + filter + "\") != -1"
+            //             }]
+            //         }, {
+            //             sort: {
+            //                 dateCreated: -1
+            //             }
+            //         }));
+            //     }
+            // }
         }
     }
 });
--- server/publications.js
+++ server/publications.js
@@ -5,6 +5,9 @@ Meteor.publish('bookmark', function(id) {
     check(id, String);
     return Bookmarks.find(id);
 });
+Meteor.publish('search', function(params, options) {
+    return Bookmarks.find(params, options);
+});
 Meteor.publish('tags', function() {
     return Tags.find();
 });
