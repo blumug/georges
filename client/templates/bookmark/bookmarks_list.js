aaef164d2355b88c895824dcd8a8b0375b3b2536
Jérôme Blondon <jerome@blondon.fr>
2014-11-07 11:39:53 +0100

    fixed search

--- client/templates/bookmark/bookmarks_list.js
+++ client/templates/bookmark/bookmarks_list.js
@@ -1,40 +1,40 @@
 Template.bookmarksList.helpers({
     bookmarks: function() {
         var filter = Session.get('searchBar');
-        if (Meteor.user()) {
-            Router.go('/' + filter);
-            // if (filter === undefined || filter === '') {
-            //     return (Bookmarks.find({
-            //         userId: Meteor.user()._id
-            //     }));
-            // } else {
-            //     if (filter.indexOf("#") != -1) {
-            //         var parsedTags = ParsedTags(filter);
-            //         return Bookmarks.find({
-            //             tags: {
-            //                 $in: parsedTags
-            //             }
-            //         }, {
-            //             sort: {
-            //                 dateCreated: -1
-            //             }
-            //         })
-            //     } else {
-            //         return (Bookmarks.find({
-            //             $or: [{
-            //                 $where: "this.url.indexOf(\"" + filter + "\") != -1"
-            //             }, {
-            //                 $where: "this.title.indexOf(\"" + filter + "\") != -1"
-            //             }, {
-            //                 $where: "this.description.indexOf(\"" + filter + "\") != -1"
-            //             }]
-            //         }, {
-            //             sort: {
-            //                 dateCreated: -1
-            //             }
-            //         }));
-            //     }
-            // }
-        }
+        return Bookmarks.find();
+        // if (Meteor.user()) {
+        //     if (filter === undefined || filter === '') {
+        //         return (Bookmarks.find({
+        //             userId: Meteor.user()._id
+        //         }));
+        //     } else {
+        //         if (filter.indexOf("#") != -1) {
+        //             var parsedTags = ParsedTags(filter);
+        //             return Bookmarks.find({
+        //                 tags: {
+        //                     $in: parsedTags
+        //                 }
+        //             }, {
+        //                 sort: {
+        //                     dateCreated: -1
+        //                 }
+        //             })
+        //         } else {
+        //             return (Bookmarks.find({
+        //                 $or: [{
+        //                     $where: "this.url.indexOf(\"" + filter + "\") != -1"
+        //                 }, {
+        //                     $where: "this.title.indexOf(\"" + filter + "\") != -1"
+        //                 }, {
+        //                     $where: "this.description.indexOf(\"" + filter + "\") != -1"
+        //                 }]
+        //             }, {
+        //                 sort: {
+        //                     dateCreated: -1
+        //                 }
+        //             }));
+        //         }
+        //     }
+        // }
     }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,47 +1,39 @@
-Meteor.publish('bookmarks', function(option) {
-    return Bookmarks.find({
-        userId: this.userId
-    }, option);
-});
-Meteor.publish('bookmark', function(id) {
-    check(id, String);
-    return Bookmarks.find(id);
-});
-Meteor.publish('search', function(filter) {
-	check(filter, String);
-    if (filter === undefined || filter === '') {
-        return (Bookmarks.find({
-            userId: this._id
-        }));
-    } else {
-        if (filter.indexOf("#") != -1) {
-            var parsedTags = ParsedTags(filter);
-            return Bookmarks.find({
-                tags: {
-                    $in: parsedTags
-                }
-            }, {
-                sort: {
-                    dateCreated: -1
-                }
-            })
-        } else {
-            return (Bookmarks.find({
-                $or: [{
-                    $where: "this.url.indexOf(\"" + filter + "\") != -1"
-                }, {
-                    $where: "this.title.indexOf(\"" + filter + "\") != -1"
-                }, {
-                    $where: "this.description.indexOf(\"" + filter + "\") != -1"
-                }]
-            }, {
-                sort: {
-                    dateCreated: -1
-                }
-            }));
+Meteor.publish('bookmarks', function(option, filter) {
+  if (!filter || filter === '') {
+    return (Bookmarks.find({
+      userId: this.userId
+    }, option));
+  } else {
+    if (filter.indexOf("#") != -1) {
+      var parsedTags = ParsedTags(filter);
+      return Bookmarks.find({
+        tags: {
+          $in: parsedTags
         }
+      }, {
+        sort: {
+          dateCreated: -1
+        }
+      })
+    } else {
+      return Bookmarks.find({
+        $or: [{
+          $where: "this.url.indexOf(\"" + filter + "\") != -1"
+        }, {
+          $where: "this.title.indexOf(\"" + filter + "\") != -1"
+        }, {
+          $where: "this.description.indexOf(\"" + filter + "\") != -1"
+        }]
+      }, options);
     }
+  }
 });
+
+Meteor.publish('bookmark', function(id) {
+  check(id, String);
+  return Bookmarks.find(id);
+});
+
 Meteor.publish('tags', function() {
-    return Tags.find();
+  return Tags.find();
 });
