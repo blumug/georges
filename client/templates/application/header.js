6152ee687ab4643d208bb82ce593822cc07c0130
flavien <flavien.pensato@gmail.com>
2014-11-13 18:42:45 +0100

    Adding btn cleanSearch

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -1,34 +1,36 @@
 var delayed;
-
 Template.header.events({
-  'keyup #tagSearch': function(events) {
-    if (delayed) {
-      clearTimeout(delayed);
-    }
-    var setFilter = function() {
-      var filter = $("#tagSearch").val();
-      Router.go('bookmarksList', {}, {
-        query: {
-          filter: filter
+    'keyup #tagSearch': function(events) {
+        if (delayed) {
+            clearTimeout(delayed);
         }
-      });
+        var setFilter = function() {
+            var filter = $("#tagSearch").val();
+            Router.go('bookmarksList', {}, {
+                query: {
+                    filter: filter
+                }
+            });
+        }
+        if ($("#tagSearch").val() == '') {
+            setFilter();
+            return;
+        }
+        delayed = _.delay(setFilter, 800);
+    },
+    'keydown #tagSearch': function(e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            return false;
+        }
+    },
+    'click #cleanSearch': function(e) {
+        e.preventDefault();
+        document.getElementById("tagSearch").value = "";
     }
-    if ($("#tagSearch").val() == '') {
-      setFilter();
-      return;
-    }
-    delayed = _.delay(setFilter, 800);
-  },
-
-  'keydown #tagSearch': function(e) {
-    if (e.keyCode == 13) {
-      e.preventDefault();
-      return false;
-    }
-  }
 });
 Template.header.helpers({
-  'searchBar': function() {
-    return Session.get("searchBar");
-  }
+    'searchBar': function() {
+        return Session.get("searchBar");
+    }
 })
