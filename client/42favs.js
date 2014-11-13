0ab113d5852c0ee0bb9a56008b9af861c2e512ec
flavien <flavien.pensato@gmail.com>
2014-11-13 17:09:25 +0100

    Adding fix bug

--- client/42favs.js
+++ client/42favs.js
@@ -1,1 +1,9 @@
 Meteor.subscribe('tags');
+if (Meteor.isClient) {
+  Deps.autorun(function() {
+    if(Accounts.loginServicesConfigured() && Meteor.userId()) {
+    	Cookie.set("meteor_userid", Meteor.userId());
+    	Cookie.set("meteor_logintoken", localStorage.getItem("Meteor.loginToken"));
+    }
+  });
+}
