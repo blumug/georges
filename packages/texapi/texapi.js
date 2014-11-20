efd010b810e3c6fe69a3abe99e1d40780c4c19f1
flavien <flavien.pensato@gmail.com>
2014-11-20 09:20:34 +0100

    create package textapi + add settings.json to gitignore

--- packages/texapi/texapi.js
+++ packages/texapi/texapi.js
@@ -1,25 +1,24 @@
 Texapi = {};
-
-Texapi.analyzeText = function(url){
-  if(!Meteor.settings.Texapi)
-    throw new Meteor.Error(500, 'Please provide a Texapi token in Meteor.settings');
-
-  var response = Meteor.http.get(
-    "http://texapi.jbl2024.com/api/v1/text/analyze/",
-    {
-      timeout: 5000,
-      params:{
-        "url": "url",
-      },
-      headers: {
-        'Authorization': 'Token ' + Meteor.settings.Texapi
-      }
+Texapi.analyzeText = function(url) {
+    if (!Meteor.settings.Texapi) throw new Meteor.Error(500, 'Please provide a Texapi token in Meteor.settings');
+    var response = Meteor.http.get("http://texapi.jbl2024.com/api/v1/text/analyze/", {
+        timeout: 5000,
+        params: {
+            "url": "url",
+        },
+        headers: {
+            'Authorization': 'Token ' + Meteor.settings.Texapi
+        }
+    });
+    if (response.statusCode === 200) {
+        //id = response.data.id
+        return response;
+    } else {
+        throw new Meteor.Error(500, "Texapi call failed with error: " + response.status_txt);
     }
-  );
-  if(response.statusCode === 200){
-    id = response.data.id
-    return id;
-  }else{
-    throw new Meteor.Error(500, "Texapi call failed with error: "+response.status_txt);
-  }
 }
+Meteor.methods({
+    'getTexapi': function(url) {
+        return Texapi.analyzeText(url);
+    }
+});
