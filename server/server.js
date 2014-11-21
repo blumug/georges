9cb43dd16b25612bba7b473533a37d0c72adb748
flavien <flavien.pensato@gmail.com>
2014-11-21 13:49:37 +0100

    Adding Admin 3

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 Meteor.startup(function() {
-    console.log("startup");
     Houston.add_collection(Bookmarks);
     Houston.add_collection(Tags);
     Houston.add_collection(Favorites);
     Houston.add_collection(Meteor.users);
     Houston.add_collection(Houston._admins);
     if (Meteor.users.find().count() === 0) {
-        console.log("createUser");
         _.each(Meteor.settings.users, function(user) {
             var id;
             id = Accounts.createUser({
