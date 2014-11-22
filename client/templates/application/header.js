a13432486c7968dce64868a6503d9ace16362968
Jérôme Blondon <jerome@blondon.fr>
2014-11-22 20:39:29 +0100

    fixed header

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -1,59 +1,41 @@
 var delayed;
 Template.header.events({
-    'keyup #tagSearch': function(events) {
-        Session.set("searchBar", $("#tagSearch").val());
-        if (delayed) {
-            clearTimeout(delayed);
-        }
-        var setFilter = function() {
-            var filter = $("#tagSearch").val();
-            Router.go('bookmarksList', {}, {
-                query: {
-                    filter: filter
-                }
-            });
-        }
-        if ($("#tagSearch").val() == '') {
-            setFilter();
-            return;
-        }
-        delayed = _.delay(setFilter, 800);
-    },
-    'click #tagSearch': function() {
-        Session.set("searchBar", $("#tagSearch").val());
-        if (delayed) {
-            clearTimeout(delayed);
-        }
-        var setFilter = function() {
-            var filter = $("#tagSearch").val();
-            Router.go('bookmarksList', {}, {
-                query: {
-                    filter: filter
-                }
-            });
-        }
-        if ($("#tagSearch").val() == '') {
-            setFilter();
-            return;
-        }
-        delayed = _.delay(setFilter, 800);
-    },
-    'click #cleanSearch': function(e) {
-        document.getElementById("tagSearch").value = "";
-        var filter = document.getElementById("tagSearch").value;
-            Router.go('bookmarksList', {}, {
-                query: {
-                    filter: filter
-                }
-            });
+  'input #tagSearch': function(e) {
+    Session.set("searchBar", $("#tagSearch").val());
+    if (delayed) {
+      clearTimeout(delayed);
     }
+    var setFilter = function() {
+      var filter = $("#tagSearch").val();
+      Router.go('bookmarksList', {}, {
+        query: {
+          filter: filter
+        }
+      });
+    }
+    if ($("#tagSearch").val() == '') {
+      setFilter();
+      return;
+    }
+    delayed = _.delay(setFilter, 800);
+  },
+  'click #cleanSearch': function(e) {
+    document.getElementById("tagSearch").value = "";
+    var filter = document.getElementById("tagSearch").value;
+    Router.go('bookmarksList', {}, {
+      query: {
+        filter: filter
+      }
+    });
+  }
 });
 Template.header.helpers({
-    'searchBar': function() {
-        return Session.get("searchBar");
-    },
-    'listFavorite': function()
-    {
-        return Favorites.find({userId: Meteor.userId()});
-    }
+  'searchBar': function() {
+    return Session.get("searchBar");
+  },
+  'listFavorite': function() {
+    return Favorites.find({
+      userId: Meteor.userId()
+    });
+  }
 })
