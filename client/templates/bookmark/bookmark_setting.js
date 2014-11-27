65723a998ab11256f6a0a6268f9cbffc7b5f4bc2
flavien <flavien.pensato@gmail.com>
2014-11-27 11:43:46 +0100

    Finished import of favorites and bookmarks

--- client/templates/bookmark/bookmark_setting.js
+++ client/templates/bookmark/bookmark_setting.js
@@ -1,14 +1,26 @@
-Template.bookmarkSetting.helpers({
-    'rendu': function() {
-        var allFavorites = Favorites.find({
-            userId: Meteor.userId()
-        }).fetch();
-        var allFavoritesString = "[";
-        for (var i = allFavorites.length - 1; i >= 0; i--) {
-            allFavoritesString += EJSON.stringify(allFavorites[i]);
-            if (i != 0) allFavoritesString += ",";
-        };
-        allFavoritesString += "]";
-        return allFavoritesString;
+Template.bookmarkSetting.events({
+    'change .import': function(events, tmp) {
+        var files = events.target.files;
+        var reader = new FileReader();
+        for (var x = 0, x; x < files.length; x++) {
+            var file = files[x];
+            reader.onload = function(events) {
+                var str = reader.result;
+                var data = EJSON.parse(str);
+                if (data) {
+                    _.each(data.bookmarks, function(bookmark) {
+                        bookmark.userId = Meteor.userId();
+                        Meteor.call("bookmarkUpdate", bookmark, "");
+                    });
+                    _.each(data.favorites, function(favorite)
+                    {
+                        Meteor.call("addToFavorites", favorite.favorite);
+                    });
+                    // alert(data.bookmarks);
+                    // alert(data.favorites);
+                }
+            }
+            reader.readAsText(file);
+        }
     }
 });
