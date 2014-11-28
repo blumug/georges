9d60877ef85d67986b5edd3e40b5f83aaa6e83a5
flavien <flavien.pensato@gmail.com>
2014-11-28 16:46:32 +0100

    Added confirm, progressBar

--- client/templates/bookmark/bookmark_setting.js
+++ client/templates/bookmark/bookmark_setting.js
@@ -1,26 +1,28 @@
 Template.bookmarkSetting.events({
     'change .import': function(events, tmp) {
-        var files = events.target.files;
-        var reader = new FileReader();
-        for (var x = 0, x; x < files.length; x++) {
-            var file = files[x];
-            reader.onload = function(events) {
-                var str = reader.result;
-                var data = EJSON.parse(str);
-                if (data) {
-                    _.each(data.bookmarks, function(bookmark) {
-                        bookmark.userId = Meteor.userId();
-                        Meteor.call("bookmarkUpdate", bookmark, "");
-                    });
-                    _.each(data.favorites, function(favorite)
-                    {
-                        Meteor.call("addToFavorites", favorite.favorite);
-                    });
-                    // alert(data.bookmarks);
-                    // alert(data.favorites);
+        if (confirm("Are you sure?", "Confirm your Importation(s)")) {
+            var files = events.target.files;
+            $("#progressBar").attr("max", files.length);
+            var reader = new FileReader();
+            for (var x = 0, x; x < files.length; x++) {
+                $("#progressBar").attr("value", x);
+                var file = files[x];
+                reader.onload = function(events) {
+                    var str = reader.result;
+                    var data = EJSON.parse(str);
+                    if (data) {
+                        _.each(data.bookmarks, function(bookmark) {
+                            bookmark.userId = Meteor.userId();
+                            Meteor.call("bookmarkUpdate", bookmark, "");
+                        });
+                        _.each(data.favorites, function(favorite) {
+                            Meteor.call("addToFavorites", favorite.favorite);
+                        });
+                    }
                 }
+                reader.readAsText(file);
             }
-            reader.readAsText(file);
+            $("#progressBar").attr("value", files.length);
         }
     }
 });
