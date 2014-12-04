33493a1bceb649b651b357e2c96451ffe51e8f95
flavien <flavien.pensato@gmail.com>
2014-12-04 18:26:06 +0100

    Fix search

--- client/templates/application/register.js
+++ client/templates/application/register.js
@@ -1,22 +1,22 @@
 Template.register.events({
-    'submit #register-form': function(e, t) {
-        e.preventDefault();
-        var email = trimInput(t.find('#account-email').value),
-            password = t.find('#account-password').value;
-        if (isValidPassword(password)) {
-            Accounts.createUser({
-                email: email,
-                password: password
-            }, function(err) {
-                if (err) {
-                    // Inform the user that account creation failed
-                } else {
-                    // Success. Account has been created and the user
-                    // has logged in successfully. 
-                    Router.go('/home');
-                }
-            });
+  'submit #register-form': function(e, t) {
+    e.preventDefault();
+    var email = trimInput(t.find('#account-email').value),
+      password = t.find('#account-password').value;
+    if (isValidPassword(password)) {
+      Accounts.createUser({
+        email: email,
+        password: password
+      }, function(err) {
+        if (err) {
+          // Inform the user that account creation failed
+        } else {
+          // Success. Account has been created and the user
+          // has logged in successfully. 
+          Router.go('/home');
         }
-        return false;
+      });
     }
+    return false;
+  }
 });
