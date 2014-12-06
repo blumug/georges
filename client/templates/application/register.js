73015736bff53fdfffceb3d39b2b2b922f27e92c
Jérôme Blondon <jerome@blondon.fr>
2014-12-06 20:23:47 +0100

    fixed register

--- client/templates/application/register.js
+++ client/templates/application/register.js
@@ -1,8 +1,23 @@
 Template.register.events({
   'submit #register-form': function(e, t) {
     e.preventDefault();
+
+    var isValidPassword = function(val, field) {
+      if (val.length >= 6) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    var trimInput = function(val) {
+      return val.replace(/^\s*|\s*$/g, "");
+    }
+
     var email = trimInput(t.find('#account-email').value),
       password = t.find('#account-password').value;
+
+
     if (isValidPassword(password)) {
       Accounts.createUser({
         email: email,
@@ -16,6 +31,8 @@ Template.register.events({
           Router.go('/home');
         }
       });
+    } else {
+      DisplayErrorSubmit("Invalid password");
     }
     return false;
   }
