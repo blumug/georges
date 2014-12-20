17e2ecdd4d73bf26a156b58c7a1a1676833ecb64
Jérôme Blondon <jerome@blondon.fr>
2014-12-20 09:43:21 +0100

    added filter group, using reactive-var package

--- client/templates/group/group_list.js
+++ client/templates/group/group_list.js
@@ -1,5 +1,26 @@
+Template.groupList.created = function () {
+  this.filterGroups = new ReactiveVar('');
+}
+
 Template.groupList.helpers({
 	'groupList': function() {
-		return Groups.find();
+    var filterGroups =  Template.instance().filterGroups.get();
+    if (filterGroups != '') {
+      var query = {
+            $regex: filterGroups,
+            $options: "i"
+      };
+		  return Groups.find({'name': query});
+    } else {
+      return Groups.find()
+    }
 	}
+});
+
+Template.groupList.events({
+  'input #filter-groups': function(event, template) {
+    event.preventDefault();
+    var val = $(event.target).val();
+    template.filterGroups.set(val);
+  }
 });
