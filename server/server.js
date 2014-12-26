9ef0c5494915912c690bb37ff11a77cb6547291a
Jérôme Blondon <jerome@blondon.fr>
2014-12-26 22:29:26 +0100

    workaround for houston add collection bug for bookmarks

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 Meteor.startup(function() {
+
     Houston.add_collection(Meteor.users);
     Houston.add_collection(Houston._admins);
+    Houston.add_collection(Notifications);
+    try {
+      Houston.add_collection(Bookmarks);
+    }
+    catch (err) {
+      
+    }
+    Houston.add_collection(Groups);
+    Houston.add_collection(Tags);
+    Houston.add_collection(Favorites);
 
     if (Meteor.users.find().count() === 0) {
         _.each(Meteor.settings.users, function(user) {
