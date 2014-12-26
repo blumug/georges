7a10cdef932d8e4804a938b9ca4b8560bab68cd9
Jérôme Blondon <jerome@blondon.fr>
2014-12-26 18:06:51 +0100

    fixed houston init

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 Meteor.startup(function() {
+    Houston.add_collection(Meteor.users);
+    Houston.add_collection(Houston._admins);
+
     if (Meteor.users.find().count() === 0) {
-        Houston.add_collection(Bookmarks);
-        Houston.add_collection(Tags);
-        Houston.add_collection(Favorites);
-        Houston.add_collection(Meteor.users);
-        Houston.add_collection(Houston._admins);
         _.each(Meteor.settings.users, function(user) {
             var id;
             id = Accounts.createUser({
