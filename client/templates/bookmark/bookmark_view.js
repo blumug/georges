5261bf86dfd435c15e890f7e3a414db90566b2d9
Jérôme Blondon <jerome@blondon.fr>
2014-12-26 22:39:59 +0100

    updated counterView

--- client/templates/bookmark/bookmark_view.js
+++ client/templates/bookmark/bookmark_view.js
@@ -1,62 +1,72 @@
 Template.bookmarkView.helpers({
-    'summary': function() {
-        return this.summary.text;
-    },
-    'readable': function() {
-        return this.summary.readable;
-    },
-  domain: function () {
+  'summary': function() {
+    return this.summary.text;
+  },
+  'readable': function() {
+    return this.summary.readable;
+  },
+  domain: function() {
     var url = this.url;
     var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
     var domain = matches && matches[1];
     return domain;
   },
-  favicon: function () {
+  favicon: function() {
     var url = this.url;
-    pathArray = url.split( '/' );
+    pathArray = url.split('/');
     var protocol = pathArray[0];
     var host = pathArray[2];
-    var domain = protocol + '//' + host; 
+    var domain = protocol + '//' + host;
     return domain + "/favicon.ico";
-  }  
+  }
 
 });
 
-Template.bookmarkView.events({
-    'click .refreshView': function(e) {
-        e.preventDefault()
-        $("#nuage").css("color", "rgb(65, 65, 178)");
-        var inc = 0;
-        var new_bookmark = this;
-        var _id = new_bookmark._id;
-        new_bookmark = _.omit(new_bookmark, '_id');
-        Meteor.call("analyzeText", new_bookmark.url, function(error, resultat) {
-            console.log(error);
-            var id = resultat;
-            var check = function(id) {
-                Meteor.call("getText", id, function(error, resultat) {
-                    console.log(error);
-                    inc++;
-                    if (inc >= 10) {
-                        console.log("analyzeText Failed");
-                        $("#nuage").css("color", "rgb(188, 0, 0)");
-                    } else if (resultat.status == "finished") {
-                        new_bookmark.summary = resultat;
-                        Meteor.call("bookmarkUpdate", new_bookmark, _id, function(error, resultat) {
-                            if (error != undefined) $("#nuage").css("color", "rgb(188, 0, 0)");
-                            else $("#nuage").css("color", "");
-                            console.log(error);
-                        });
-                    } else {
-                        Meteor.setTimeout(function() {
-                            check(id);
-                        }, 4000);
-                    }
-                });
-            }
-            Meteor.setTimeout(function() {
-                check(id);
-            }, 4000);
-        });
+Template.bookmarkView.rendered = function(template) {
+  Bookmarks.update({
+    _id: Template.instance().data._id
+  }, {
+    $inc: {
+      counterView: 1
     }
+  });
+}
+
+Template.bookmarkView.events({
+  'click .refreshView': function(e) {
+    e.preventDefault()
+    $("#nuage").css("color", "rgb(65, 65, 178)");
+    var inc = 0;
+    var new_bookmark = this;
+    var _id = new_bookmark._id;
+    new_bookmark = _.omit(new_bookmark, '_id');
+    Meteor.call("analyzeText", new_bookmark.url, function(error, resultat) {
+      console.log(error);
+      var id = resultat;
+      var check = function(id) {
+        Meteor.call("getText", id, function(error, resultat) {
+          console.log(error);
+          inc++;
+          if (inc >= 10) {
+            console.log("analyzeText Failed");
+            $("#nuage").css("color", "rgb(188, 0, 0)");
+          } else if (resultat.status == "finished") {
+            new_bookmark.summary = resultat;
+            Meteor.call("bookmarkUpdate", new_bookmark, _id, function(error, resultat) {
+              if (error != undefined) $("#nuage").css("color", "rgb(188, 0, 0)");
+              else $("#nuage").css("color", "");
+              console.log(error);
+            });
+          } else {
+            Meteor.setTimeout(function() {
+              check(id);
+            }, 4000);
+          }
+        });
+      }
+      Meteor.setTimeout(function() {
+        check(id);
+      }, 4000);
+    });
+  }
 });
