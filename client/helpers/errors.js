c8d6ab872aff639a48186ee5e67c0b8f04257e5f
Jérôme Blondon <jerome@blondon.fr>
2014-12-27 23:51:40 +0100

    added toastr for notifications

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -9,20 +9,10 @@ throwError = function(message) {
 
 
 DisplayErrorSubmit = function(message) {
-  var e = $('<div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button><h4>Warning!</h4><p>' + message + '</p></div>');
-  $("#alertConnexion").append(e);
-  e.attr('id', 'myid');
-  setTimeout(function() {
-    $("#alertConnexion").remove(".alert");
-  }, 3000);
+  toastr.warning(message);
 }
 
 
 DisplaySuccessSubmit = function(message) {
-  var e = $('<div class="alert alert-dismissable alert-success"><button type="button" class="close" data-dismiss="alert">×</button><h4>Success!</h4><p>' + message + '</p></div>');
-  $("#alertConnexion").append(e);
-  e.attr('id', 'myid');
-  setTimeout(function() {
-    $("div").remove(".alert");
-  }, 3000);
+  toastr.success(message);
 }
