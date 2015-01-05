7dd4281491907d2e3190459f7298fdb2edd3ab94
Jérôme Blondon <jerome@blondon.fr>
2015-01-05 14:54:41 +0100

    put houston init in try/catch

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 Meteor.startup(function() {
-
-    Houston.add_collection(Meteor.users);
-    Houston.add_collection(Houston._admins);
-    Houston.add_collection(Notifications);
     try {
+      Houston.add_collection(Meteor.users);
+      Houston.add_collection(Houston._admins);
+      Houston.add_collection(Notifications);
       Houston.add_collection(Bookmarks);
+      Houston.add_collection(Groups);
+      Houston.add_collection(Tags);
+      Houston.add_collection(Favorites);
     }
     catch (err) {
       
     }
-    Houston.add_collection(Groups);
-    Houston.add_collection(Tags);
-    Houston.add_collection(Favorites);
 
     if (Meteor.users.find().count() === 0) {
         _.each(Meteor.settings.users, function(user) {
