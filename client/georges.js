4c84d2f7b7224b6fe7a307d7eccf29b2f9ee013d
neywen <neywen@neywen.net>
2015-05-17 17:26:51 +0200

    fixed bug "chrome tab is always opened on georges domain the first time the georges plugin icon is clicked, even if the user is already loggued in" : removed the chuangbo:cookie package to avoid conflicts with the package mrt:cookies (it does not allow to set a proper duration to a cookie), added a duration of 10 years to the cookies "meteor_userid" and "meteor_logintoken" (without a duration, a cookie is a session cookie, and therefore is automatically deleted when the browser is closed)

--- client/georges.js
+++ client/georges.js
@@ -1,10 +1,14 @@
 Meteor.subscribe('tags');
 Meteor.subscribe('favorites');
+
 if (Meteor.isClient) {
+
 	Deps.autorun(function() {
-		if (Accounts.loginServicesConfigured() && Meteor.userId()) {
-			Cookie.set("meteor_userid", Meteor.userId());
-			Cookie.set("meteor_logintoken", localStorage.getItem("Meteor.loginToken"));
+
+    if (Accounts.loginServicesConfigured() && Meteor.userId()) {
+
+      Cookie.set("meteor_userid", Meteor.userId(), {"years":10});
+			Cookie.set("meteor_logintoken", localStorage.getItem("Meteor.loginToken"), {"years":"10"});
 		}
 	});
 }
