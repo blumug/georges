2684fac7097424759f6eb729f47a5ef09742a2e1
Jérôme Blondon <jerome@blondon.fr>
2015-06-05 21:35:02 +0200

    sort notification by -date and do not limit to 10 notifs

--- server/publications/notifications.js
+++ server/publications/notifications.js
@@ -1,5 +1,11 @@
 Meteor.publish('notifications', function() {
+  var options = {
+    sort: {
+      date: -1
+    }
+  };
+  
   return Notifications.find({
     receiver: this.userId
-  });
+  }, options);
 });
